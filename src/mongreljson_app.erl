c55dd8200a3bcae907d098f7fb4e45d49cb161de
ubuntu32 <liuyang@ubuntu.(none)>
2012-10-09 17:27:21 +0800

    rebar

---
+++ src/mongreljson_app.erl
@@ -0,0 +1,16 @@
+-module(mongreljson_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    mongreljson_sup:start_link().
+
+stop(_State) ->
+    ok.
